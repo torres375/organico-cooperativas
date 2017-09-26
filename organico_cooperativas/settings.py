4f84f49236534e22e02aa5e95fe0abc2e505d37b
Unknown <ad.betin@uniandes.edu.co>
2017-09-26 13:23:12 -0500

    Configuracion heroku archivos estaticos

--- organico_cooperativas/settings.py
+++ organico_cooperativas/settings.py
@@ -28,13 +28,17 @@ DEBUG = True
 ALLOWED_HOSTS = ['organico-cooperativas.herokuapp.com', 'localhost', '127.0.0.1']
 
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # Application definition
 
