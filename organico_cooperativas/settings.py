8dc35d85e6378b14fd6de5dfde46ccfc899428f4
Unknown <ad.betin@uniandes.edu.co>
2017-09-26 13:20:21 -0500

    Configuracion para archivos estaticos

--- organico_cooperativas/settings.py
+++ organico_cooperativas/settings.py
@@ -29,19 +29,12 @@ ALLOWED_HOSTS = ['organico-cooperativas.herokuapp.com', 'localhost', '127.0.0.1'
 
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATIC_URL = '/static/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 # Application definition
 
